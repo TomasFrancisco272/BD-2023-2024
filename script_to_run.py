5102984dba05c11b121b3fad020a974d57d6132a
Tomás Francisco A93193 <pg54263@alunos.uminho.pt>
2024-06-10 15:37:16 +0100

    Script Finished

--- script_to_run.py
+++ script_to_run.py
@@ -5,14 +5,19 @@ def run_cypher_script(uri, user, password, script_path):
     with driver.session() as session:
         with open(script_path, 'r') as file:
             cypher_script = file.read()
-            result = session.run(cypher_script)
-            for record in result:
-                print(record)
+            statements = cypher_script.split(';')
+            for statement in statements:
+                if statement.strip():  # Execute only if the statement is not empty
+                    result = session.run(statement)
+                    for record in result:
+                        print(record)
 
 # Set your connection details
-uri = "bolt://localhost:7687"  # Change if needed
+uri = "bolt://localhost:7687"
 user = "neo4j"
 password = "abcd1234"
 script_path = "hospital_v2.cypher"
 
 run_cypher_script(uri, user, password, script_path)
+
+print("MISSION: COMPLETE\n")
